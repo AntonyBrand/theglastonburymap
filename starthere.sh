7523c7307f5074b38cfa8a65db151108f8a599d0
AntonyBrand <antonybrand@Antonys-MacBook-Pro.local>
2018-06-14 08:29:18 +0100

    editing bash script to download using curl -O

--- starthere.sh
+++ starthere.sh
@@ -3,8 +3,15 @@
 # Header is in the first row : id,postcode,latitude,longitude
 # https://www.freemaptools.com/download/full-postcodes/ukpostcodes.zip
 
+# or for the small version
+
+curl -O https://www.freemaptools.com/download/outcode-postcodes/postcode-outcodes.csv
 
 
+
+
+
+# Thoughts after meeting with Jay and Topher
 
 # maybe using google arcore:
 # https://developers.google.com/ar/develop/developer-guides/anchors
@@ -12,3 +19,11 @@
 
 # If I extend this to an android app I'll need:
 # https://developer.android.com/studio/
+
+# and three.js
+# https://threejs.org/examples/#webgl_animation_scene
+
+# maybe a marine gps to get data logged info including heigt information
+# maybe using OS maps from OpenStreetMap?
+# or the Google Maps Elevation API
+# https://developers.google.com/maps/documentation/elevation/intro
